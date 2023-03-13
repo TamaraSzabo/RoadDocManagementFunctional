f7b30cd7558d262ea17eebfc111d308ee6a77b94
Szabo Tamara <tamaraszabo19@gmail.com>
2023-03-13 22:23:28 +0200

    Drawer functionality added

--- app/src/main/java/com/example/roaddocmanagement/activities/MainActivity.kt
+++ app/src/main/java/com/example/roaddocmanagement/activities/MainActivity.kt
@@ -1,12 +1,103 @@
 package com.example.roaddocmanagement.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.UserHandle
+import android.view.MenuItem
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.bumptech.glide.Glide
 import com.example.roaddocmanagement.R
+import com.example.roaddocmanagement.firebase.FirestoreClass
+import com.example.roaddocmanagement.models.User
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
+import de.hdodenhof.circleimageview.CircleImageView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupActionBar()
+        val navView = findViewById<NavigationView>(R.id.nav_view)
+        navView.setNavigationItemSelectedListener(this)
+        val drawerLayout =
+            findViewById<DrawerLayout>(R.id.drawer_layout)
+
+        FirestoreClass().signInUser(this)
+
+    }
+
+    private fun setupActionBar() {
+        val toolbarMainActivity =
+            findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar_main_activity)
+        setSupportActionBar(toolbarMainActivity)
+        toolbarMainActivity.setNavigationIcon(R.drawable.ic_action_navigation_menu)
+
+        toolbarMainActivity.setNavigationOnClickListener {
+            toggleDrawer()
+        }
+    }
+
+    private fun toggleDrawer() {
+        if (::drawerLayout.isInitialized) {
+            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                drawerLayout.closeDrawer(GravityCompat.START)
+            } else {
+                drawerLayout.openDrawer(GravityCompat.START)
+            }
+        } else {
+            drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                drawerLayout.closeDrawer(GravityCompat.START)
+            } else {
+                drawerLayout.openDrawer(GravityCompat.START)
+            }
+        }
+
+        fun onBackPressed() {
+            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                drawerLayout.closeDrawer(GravityCompat.START)
+            } else {
+                doubleBackToExit()
+            }
+        }
+    }
+
+    fun updateNavigationUserDetails(user: User) {
+        val navUserImage = findViewById<CircleImageView>(R.id.nav_user_image)
+        Glide
+            .with(this)
+            .load(user.image)
+            .centerCrop()
+            .placeholder(R.drawable.ic_user_place_holder)
+            .into(navUserImage)
+
+        val tvUserName = findViewById<TextView>(R.id.tv_username)
+        tvUserName.text = user.name
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_my_profile -> {
+                Toast.makeText(this, "My profile", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_sign_out -> {
+                FirebaseAuth.getInstance().signOut()
+
+                val intent = Intent(this, IntroActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+                finish()
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+
+        return true
     }
 }
--- app/src/main/java/com/example/roaddocmanagement/firebase/FirestoreClass.kt
+++ app/src/main/java/com/example/roaddocmanagement/firebase/FirestoreClass.kt
@@ -1,6 +1,8 @@
 package com.example.roaddocmanagement.firebase
 
+import android.app.Activity
 import android.util.Log
+import com.example.roaddocmanagement.activities.MainActivity
 import com.example.roaddocmanagement.activities.RegisterActivity
 import com.example.roaddocmanagement.activities.SignInActivity
 import com.example.roaddocmanagement.models.User
@@ -23,15 +25,31 @@ class FirestoreClass {
             }
     }
 
-    fun signInUser(activity: SignInActivity) {
+    fun signInUser(activity: Activity) {
         mFireStore.collection(Constants.USERS)
             .document(getCurrentUserId())
             .get()
             .addOnSuccessListener { document ->
-                val loggedInUser = document.toObject(User::class.java)
-                if (loggedInUser != null)
-                    activity.signInSuccess(loggedInUser)
+                val loggedInUser = document.toObject(User::class.java)!!
+
+                when (activity) {
+                    is SignInActivity -> {
+                        activity.signInSuccess(loggedInUser)
+                    }
+                    is MainActivity -> {
+                        activity.updateNavigationUserDetails(loggedInUser)
+                    }
+                }
+
             }.addOnFailureListener { e ->
+                when (activity) {
+                    is SignInActivity -> {
+                        activity.hideProgressDialog()
+                    }
+                    is MainActivity -> {
+                        activity.hideProgressDialog()
+                    }
+                }
                 Log.e(activity.javaClass.simpleName, "Error registering the account")
             }
 
@@ -40,9 +58,10 @@ class FirestoreClass {
     fun getCurrentUserId(): String {
         var currentUser = FirebaseAuth.getInstance().currentUser
         var currentUserId = ""
-            if (currentUser != null) {
-                currentUserId = currentUser.uid
-            }
+
+        if (currentUser != null) {
+            currentUserId = currentUser.uid
+        }
         return currentUserId
     }
 }
